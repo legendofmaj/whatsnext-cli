0924f948a95ab9214a850c922ca11d22856be69a
legendofmaj <Michaelalexanderjung@proton.me>
2024-11-17 21:45:28 +0100

    feat: get type of list
    the current implementation doesn't work if the folder is empty

--- main.cpp
+++ main.cpp
@@ -3,11 +3,14 @@
 #include <map>
 #include <fstream>
 #include <sys/stat.h> //for mkdir
+#include <filesystem> //to list files in a directory
 
 using namespace std;
+namespace fs = std::filesystem;
 
 int genRand(int max);
 //save system
+void getFileName();
 void checkForSave();
 void writeFile();
 void getFileLength();
@@ -20,7 +23,7 @@ string* strings;
 class SaveSystem
 {
     public:
-        string filename = "config.txt";
+        string inputtype;
         const char* filepath = "/home/michi/Documents/.whatsnext-config/";
         string line;
         bool fileExists = false;
@@ -40,6 +43,7 @@ int main()
     }
     else
     {
+        getFileName();
         getFileLength();
         readFile(generalInfo.fileLength);
     }
@@ -53,11 +57,36 @@ int main()
     return 0;
 }
 
+void getFileName()
+{
+    int i = 0;
+    string pathName;
+    
+    for (const auto & entry : fs::directory_iterator(generalInfo.filepath))
+    {
+        //cout << entry.path() << endl; //output files in the directory
+        pathName = entry.path();
+        i++;
+    }
+    pathName.erase(pathName.begin(), pathName.begin()+40); //delete the first 40 letters (leave only filename)
+    cout << "Your current config is " << pathName << endl;
+    cout << "There is/are " << i -1 << " other files in the config folder. Their usage will be implemented later" << endl;
+    generalInfo.inputtype = pathName;
+}
+
 void checkForSave()
 {
-    ifstream saveFile(generalInfo.filepath + generalInfo.filename);
-    generalInfo.fileExists = saveFile.is_open();
-    saveFile.close();
+    //limitation: doesn't work if the folder is empty
+    struct stat sb;
+    if (stat(generalInfo.filepath, &sb) == 0) 
+    {
+        //check if folder is empty here
+        generalInfo.fileExists = true;
+    }
+    else 
+    {
+        generalInfo.fileExists = false;
+    }
 }
 
 void writeFile()
@@ -65,7 +94,6 @@ void writeFile()
     string line;
 
 	cout << "No save data exists yet." << endl;
-	cout << "First enter the type of string you whish to use, then the names of each string" << endl;
 	cout << "If you have entered all your strings, type esc to stop!" << endl;
 
     //create folder
@@ -73,8 +101,13 @@ void writeFile()
     {
         cout << "Failed to create folder." << endl;
     }
+    //get name
+    cout << "Enter the type of input you wish to use ";
+    cin >> generalInfo.inputtype;
 
-	ofstream saveFile(generalInfo.filepath + generalInfo.filename);
+    cout << "Now enter the names of each " << generalInfo.inputtype << endl;
+
+	ofstream saveFile(generalInfo.filepath + generalInfo.inputtype);
 	while(true)
 	{
 		cin >> line;
@@ -91,7 +124,7 @@ void getFileLength()
 {
 	string line;
 
-	ifstream saveFile(generalInfo.filepath + generalInfo.filename);
+	ifstream saveFile(generalInfo.filepath + generalInfo.inputtype);
 	while(getline(saveFile, line)) //As soon as no new lines can be read the function is quit
 	{
 		generalInfo.fileLength++;
@@ -107,7 +140,7 @@ void readFile(int numStrings)
 	//Allocate memory
 	strings = new string[numStrings];
 
-	ifstream saveFile(generalInfo.filepath + generalInfo.filename);
+	ifstream saveFile(generalInfo.filepath + generalInfo.inputtype);
 	while(getline(saveFile, line))
 	{
 		strings[i] = line;
@@ -154,7 +187,7 @@ int genRand(int max)
     //print map
     for(auto i : mapping)
     {
-        cout << "Your " << i.first << ". " << "string type (to be added)" << " is " << i.second << endl;
+        cout << "Your " << i.first << ". " << generalInfo.inputtype << " is " << i.second << endl;
     }
     return 0;
 }
@@ -176,6 +209,7 @@ void mainMenu()
             cout << "\e[4mlist of help commands\e[0m: " << endl;
             cout << "help -> get a list of commands" << endl;
             cout << "exit -> quit the program" << endl;
+            cout << "clear -> clear the terminal" << endl;
             cout << "start -> start the program with the default configuration" << endl;
             cout << "\e[1mmore commands will be added in the future\e[0m" << endl;
             cin >> in;
@@ -188,6 +222,10 @@ void mainMenu()
         if (in == "start")
         {
             break;
+        }
+        if (in == "clear")
+        {
+            cout << "\033[2J\033[1;1H";
         }
         //ToDo: add valid input check
     }
